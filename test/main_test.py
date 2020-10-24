488a380a2f6b69f5279a12f84689d5bc32ded00f
Zach Siegel <siegelzc@gmail.com>
2020-10-24 16:08:20 -0500

    fix package structure

--- test/main_test.py
+++
@@ -1,9 +0,0 @@
-from src import app
-
-def test_index():
-    app.testing = True
-    client = app.test_client()
-
-    r = client.get('/')
-    assert r.status_code == 200
-    assert 'hello world' in r.data.decode('utf-8')
