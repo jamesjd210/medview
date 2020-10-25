091cab0bb8d67035c4d5a0a5ec034e901e296ea0
Zach Siegel <siegelzc@gmail.com>
2020-10-24 22:00:56 -0500

    fix home page select

---
+++ src/config.py
@@ -0,0 +1,13 @@
+provider_types = [
+    'All',
+    'Hospital',
+    'Dentistry',
+    'Primary Care Clinic',
+    'Urgent Care Clinic',
+    'Convenience Clinic',
+    'Imaging Center',
+    'Laboratory',
+    'Supplier',
+    'Specialty Center',
+    'Other'
+]
