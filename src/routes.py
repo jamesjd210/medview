4a60b8310a8e1a0e979853da41c3794fa56a4335
AshrayDesai <ashray@LAPTOP-MVAQHP79.localdomain>
2020-10-25 03:34:43 -0500

    Create backend map marker placement functionality, updated routing to include queries

--- src/routes.py
+++ src/routes.py
@@ -8,3 +8,9 @@ from .config import institution_types, insurer_types
 @app.route('/', methods=['GET'])
 def home_page():
     return render_template('home_page.html', health_institutions=institution_types, insurance_providers=insurer_types)
+
+@app.route('query?name_insurer=<name_insurer>&type_institution=<type_institution>', method=['GET'])
+def get_user_params(name_insurer=None, type_institution=None):
+    if name_insurer is None or type_institution is None:
+        return
+    return [];
