49e23b3a5cc81ff22d350c6c3f7b1e34c1108c19
albertchokor <albert.cho.kor@gmail.com>
2020-10-25 02:03:29 -0700

    Fixed flask routing between home and about pages.

--- src/routes.py
+++ src/routes.py
@@ -8,3 +8,9 @@ from .config import institution_types, provider_types
 @app.route('/', methods=['GET'])
 def home_page():
     return render_template('home_page.html', health_institutions=institution_types, insurance_providers=provider_types)
+@app.route('/about_page', methods=['GET'])
+def about_page():
+    return render_template('about_us.html')
+@app.route('/home_page', methods=['GET'])
+def home_back():
+    return render_template('home_page.html', health_institutions=institution_types, insurance_providers=provider_types)
