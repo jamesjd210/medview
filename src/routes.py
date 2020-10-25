235ed411ac71a1f6b6c2510e74d2b82e34852080
jamesjd210 <jamesjd210@JJDT470P.localdomain>
2020-10-24 21:23:53 -0500

    update variables

--- src/routes.py
+++ src/routes.py
@@ -6,6 +6,6 @@ from . import app
 
 @app.route('/', methods=['GET'])
 def home_page():
-    health_insurance = ['All', 'Hospital', 'Dentistry', 'Primary Care Clinic', 'Urgent Care Clinic', 'Convenience Clinic', 'Imaging Center',
+    health_institution = ['All', 'Hospital', 'Dentistry', 'Primary Care Clinic', 'Urgent Care Clinic', 'Convenience Clinic', 'Imaging Center',
     'Laboratory', 'Supplier', 'Specialty Center', 'Other']
-    return render_template('home_page.html', health_insurance=health_insurance)
+    return render_template('home_page.html', health_institution=health_institution)
